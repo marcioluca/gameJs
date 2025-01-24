fba6ce4bf8e0cf03ede021d4138273bd044457ac
Marcio lucas <lucasmrc2014@gmail.com>
2025-01-24 20:07:26 -0300

    projeto inicial teste

---
+++ app.js
@@ -0,0 +1,19 @@
+// Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+console.log(listaGenerica);
+// Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com 
+// os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ["JavaScript", "C", "C++", "Kotlin", "Phyton"];
+console.log(linguagensDeProgramacao);
+// Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push("Java", "Ruby", "Golang");
+console.log(linguagensDeProgramacao);
+// Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let listaPessoas = ["João", "Jorge", "José"];
+console.log(listaPessoas[0]);
+// Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+let listaPessoas2 = ["Cadu", "Dudu", "Edu"]
+console.log(listaPessoas2[1]);
+// Crie uma lista com 3 nomes e exiba no console apenas o último elemento
+let listaPessoas3 = ["Luke", "Lucy", "Leonard"];
+console.log(listaPessoas3[2])
